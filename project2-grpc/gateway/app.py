89bd95a06195deaad8469baa133385941fef93eb
thiago vivan <thiago-vivanfsa@hotmail.com>
2025-05-04 22:26:46 -0300

    final commit

--- project2-grpc/gateway/app.py
+++ project2-grpc/gateway/app.py
@@ -6,13 +6,12 @@ import textprocessor_pb2_grpc
 app = Flask(__name__)
 
 # Endereço dos microserviços (ajuste conforme sua rede/infra)
-A_ADDRESS = 'localhost:50051'  # Go server
-B_ADDRESS = 'localhost:50052'  # Ruby server
+A_ADDRESS = '10.0.0.2:50051'  # Go server
+B_ADDRESS = '10.0.0.3:50052'  # Ruby server
 
 @app.route('/count-words', methods=['POST'])
 def count_words():
-    data = request.get_json()
-    text = data.get('text', '')
+    text = (request.get_json(silent=True) or {}).get('text') or request.form.get('text', '')    
     with grpc.insecure_channel(A_ADDRESS) as channel:
         stub = textprocessor_pb2_grpc.TextProcessorAStub(channel)
         response = stub.CountWords(textprocessor_pb2.TextRequest(text=text))
@@ -20,8 +19,7 @@ def count_words():
 
 @app.route('/count-characters', methods=['POST'])
 def count_characters():
-    data = request.get_json()
-    text = data.get('text', '')
+    text = (request.get_json(silent=True) or {}).get('text') or request.form.get('text', '')    
     with grpc.insecure_channel(B_ADDRESS) as channel:
         stub = textprocessor_pb2_grpc.TextProcessorBStub(channel)
         response = stub.CountCharacters(textprocessor_pb2.TextRequest(text=text))
@@ -31,11 +29,11 @@ def count_characters():
 def index():
     return '''
     <h1>API Gateway (P)</h1>
-    <form action="/count-words" method="post" enctype="application/json">
+    <form action="/count-words" method="post" >
         <input name="text" placeholder="Digite o texto">
         <button type="submit">Contar Palavras</button>
     </form>
-    <form action="/count-characters" method="post" enctype="application/json">
+    <form action="/count-characters" method="post">
         <input name="text" placeholder="Digite o texto">
         <button type="submit">Contar Caracteres</button>
     </form>
--- project2-grpc/serverA/go.mod
+++ project2-grpc/serverA/go.mod
@@ -13,3 +13,4 @@ require (
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
 )
+ 
